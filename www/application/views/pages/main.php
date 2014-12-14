204f6284c30521cc4846c51178bec470fcaa711d
protectedfor <protected.for@gmail.com>
2014-12-14 19:33:17 +0600

    Форма онлайн бронирования

--- www/application/views/pages/main.php
+++ www/application/views/pages/main.php
@@ -1,6 +1,63 @@
-<div class="avia-block">
 
+<div class="avia-block">
+    <div class="avia-block-inner">
+        <h2>Онлайн бронироавние билетов</h2>
+            <input type="checkbox" name="option1" value="a1">
+          <span class="checkbox">В одну сторону</span>
+            <input type="checkbox" name="option2" value="a2">
+          <span class="checkbox">Туда и обратно</span>
+            <input type="checkbox" name="option3" value="a3">
+          <span class="checkbox">Составить маршрут</span>
+            <div class="clear"></div>
+        <div class="avia-block-item avia-block-item-1">
+            <label for="">Город вылета</label>
+            <input type="text">
+        </div>
+            <span class="form_marker"></span>
+        <div class="avia-block-item avia-block-item-2">
+            <label for="">Город назначения</label>
+            <input type="text">
+        </div>
+            <div class="clear"></div>
+        <div class="avia-block-item avia-block-item-3">
+            <label for="">Туда</label>
+            <input type="text">
+        </div>
+        <div class="avia-block-item avia-block-item-4">
+            <label for="">Обратно</label>
+            <input type="text">
+        </div>
+            <div class="clear"></div>
+        <div class="passangers">
+            <p>Количество пассажиров</p>
+            <div class="passangers_item passangers_item_1">
+                <span></span>
+                <div class="passangers_field">0</div>
+                <a href="" class="passangers_more"></a>
+                <a href="" class="passangers_less"></a>
+            </div>
+            <div class="passangers_item passangers_item_2">
+                <span></span>
+                <div class="passangers_field">0</div>
+                <a href="" class="passangers_more"></a>
+                <a href="" class="passangers_less"></a>
+            </div>
+            <div class="passangers_item passangers_item_3">
+                <span></span>
+                <div class="passangers_field">0</div>
+                <a href="" class="passangers_more"></a>
+                <a href="" class="passangers_less"></a>
+            </div>
+        </div>
+        <input type="submit" class="economy_class" value="Эконом">
+        <input type="submit" class="business_class" value="Бизнесс">
+            <div class="clear"></div>
+        <input type="checkbox" class="checkbox_bottom" name="option2" value="a5">
+        <span class="checkbox">Показать отели в соседней вкладке</span>
+            <a href="#" class="blue_find_btn">Найти</a>
+    </div>
 </div>
+
 <div class="main-container">
     <?= Modules::run('tours/view', true) ?>
     <div class="search-hotel-block">
