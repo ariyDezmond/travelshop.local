872e8d8165785d074d5cc92794726cae54c42d5a
Andrey <protected.for@gmail.com>
2015-02-27 02:33:36 +0600

    Форма бронирования туров (не до конца)

--- www/application/views/pages/main.php
+++ www/application/views/pages/main.php
@@ -37,7 +37,7 @@
 
 <div class="search_tour">
     <div class="wrapper">
-        <iframe src="http://spb.ntk-intourist.ru/main.aspx?userCSS=<?= base_url() ?>css/ex2.css" width="100%" height="100%"></iframe>
+        <iframe src="http://spb.ntk-intourist.ru/main.aspx?userCSS=<?= base_url() ?>css/form.css" width="100%" height="100%"></iframe>
     </div><!-- /wrapper -->
 </div><!-- /search_tour -->
 
@@ -78,31 +78,3 @@
 <div class="partners">
     <?= Modules::run('partners/view', true) ?>
 </div><!-- /partners -->
-
-
-
-
-<!--<div class="avia-block">
-    <div class="avia-block-inner">
-        <script charset="utf-8" async="true" src="//www.travelpayouts.com/widgets/aaf797c0f8cac524e381229d52f16774.js?v=329"></script>
-    </div>
-</div>
-<div class="main-container">
-<? Modules::run('tours/view', true) ?>
-    <div class="search-hotel-block">
-        <h1 class="block-title">Найти отель</h1>
-        <div class="hotel-desc">
-            <img src="img/img.png" alt=""/>
-            <p class="title">Оставьте заявку и наш менеджер
-                свяжется с Вами, как только найдет
-                подходящий отель для Вас.</p>
-            <p class="dd">
-                Теперь Вам не нужно самому искать отели,
-                доверьте эту работу нам, Вам нужно только
-                указать параметры отеля, а мы позаботимся
-                об остальном сами.
-            </p>
-        </div>
-<? Modules::run('requests/save'); ?>
-    </div>
-<? Modules::run('partners/view', true) ?>-->
